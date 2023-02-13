96d210045a64c2a32fc9cf04d39498a35fe8a2f4
UnLuckyLust <104845736+UnLuckyLust@users.noreply.github.com>
2023-02-13 23:46:31 +0200

    Create fxmanifest.lua

---
+++ ull-announcer/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'adamant'
+game 'gta5'
+lua54 'yes'
+
+author 'UnLuckyLust#9534'
+description 'Automated Announcements for QBcore framework'
+version '1.2.0'
+
+shared_script '/config.lua'
+
+server_scripts {
+	'/server.lua',
+}
+client_scripts {
+	'/client.lua',
+}
